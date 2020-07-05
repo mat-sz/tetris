db216bd6cdb19ddfb0fd3811d9ee13a46c2d3319
Mat Sz <contact@matsz.dev>
2020-07-05 16:07:56 +0200

    added canvas sizing

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import './App.scss';
 
+import { boardHeight, boardWidth, boxWidth } from './constants';
+
 const canvas: HTMLCanvasElement = document.getElementById(
   'canvas'
 ) as HTMLCanvasElement;
+
+canvas.width = boardWidth * boxWidth;
+canvas.height = boardHeight * boxWidth;
+
 const ctx = canvas.getContext('2d');
 
 const resetGameButton = document.getElementById('reset-game');
