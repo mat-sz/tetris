1ee3ea761a870764ff6a6be66b2ca377f8249903
Mat Sz <contact@matsz.dev>
2020-07-06 19:12:57 +0200

    added board and drawing

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ const canvas: HTMLCanvasElement = document.getElementById(
 canvas.width = boardWidth * boxWidth;
 canvas.height = boardHeight * boxWidth;
 
+const board = new Array(boardHeight * boardWidth).fill(0);
+
 const ctx = canvas.getContext('2d');
 
 const resetGameButton = document.getElementById('reset-game');
@@ -21,3 +23,26 @@ document.addEventListener('gesturestart', e => {
   // Disable zoom on mobile Safari.
   e.preventDefault();
 });
+
+const draw = () => {
+  ctx.fillStyle = '#000000';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = 'red';
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === 0) {
+      continue;
+    }
+
+    const x = i % boardWidth;
+    const y = Math.floor(i / boardWidth);
+
+    const canvasX = x * boxWidth;
+    const canvasY = y * boxWidth;
+    ctx.fillRect(canvasX, canvasY, boxWidth, boxWidth);
+  }
+
+  requestAnimationFrame(draw);
+};
+
+requestAnimationFrame(draw);
